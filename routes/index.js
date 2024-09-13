e35db6d583ec96ffcb360a40b807d259958b6c6e
mzhafranh <88304603+mzhafranh@users.noreply.github.com>
2024-09-13 15:27:07 +0700

    register, todos, add, and edit appearance done

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,59 @@ var express = require('express');
 const { route } = require('./users');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
-
 module.exports = function (db) {
+
+  // const filter = {name: req.query.name,
+  //   height: req.query.height,
+  //   weight: req.query.weight,
+  //   startDate: req.query.startDate,
+  //   endDate: req.query.endDate,
+  //   married: req.query.married,
+  //   operation: req.query.operation
+  // }
+
+
+  router.get('/', function (req, res, next) {
+    res.render('index');
+  });
+
+  router.get('/register', function (req, res, next) {
+    res.render('register');
+  });
+
+  router.get('/test', async function (req, res, next) {
+
+    const page = 1
+    const pages = 1
+
+    // const filter = {
+    //   title: "Wululu",
+    //   complete: '1',
+    //   startDate: "2000-01-01",
+    //   endDate: "2020-01-01",
+    //   operation: "OR"
+    // }
+
+    const filter = {
+      title: "",
+      complete: '',
+      startDate: "",
+      endDate: "",
+      operation: ""
+    }
+
+    const filterPage = `&page=1&title=Wululu&complete=1&startDate=2000-01-01&endDate=2020-01-01&operation=OR`
+  
+    let sql = 'SELECT * FROM todos';
+  
+    db.query(sql, (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log(data.rows);
+      res.render('test', { rows: data.rows, pages, page, filter, filterPage })
+    })
+  });
 
   return router;
 }
--- routes/todos.js
+++ routes/todos.js
@@ -4,33 +4,33 @@ var router = express.Router();
 // module.exports = router;
 module.exports = function (db) {
 
-  function add(name, height, weight, birthdate, married, callback) {
-    db.run('INSERT INTO data (name, height, weight, birthdate, married) VALUES (?, ?, ?, ?, ?)', [name, height, weight, birthdate, married], (err) => {
+  function add(title, complete, deadline, userid, callback) {
+    db.query('INSERT INTO todos (title_column, complete_column, deadline_column, userid_column) VALUES ($1, $2, $3, $4)', [title, complete, deadline, userid], (err) => {
       callback(err);
     });
   }
 
   function select(id, callback) {
-    db.all('SELECT * FROM data WHERE id = ?', [id], (err, data) => {
+    db.query('SELECT * FROM todos WHERE id = $1', [id], (err, data) => {
       // console.log(data)
       callback(err, data);
     })
   }
 
-  function update(id, name, height, weight, birthdate, married, callback) {
-    db.run('UPDATE data SET name = ?, height = ?, weight = ?, birthdate = ?, married = ? WHERE id = ?', [name, height, weight, birthdate, married, id], (err) => {
+  function update(title, complete, deadline, userid, id, callback) {
+    db.query('UPDATE todos SET title_column = $1, complete_column = $2, deadline_column = $3, userid_column = $4 WHERE id = $5', [title, complete, deadline, userid, id], (err) => {
       callback(err);
     });
   }
 
   function remove(id, callback) {
-    db.run('DELETE FROM data WHERE id = ?', [id], (err) => {
+    db.query('DELETE FROM todos WHERE id = $1', [id], (err) => {
       callback(err);
     })
   }
 
   /* GET home page. */
-  router.get('/', function (req, res, next) {
+  router.get('/', async function (req, res, next) {
     const page = req.query.page || 1;
     const limit = 5;
     const offset = (page - 1) * limit;
@@ -38,59 +38,47 @@ module.exports = function (db) {
     const values = []
     const filterPageArray = []
     var filterPage = ``
+    var count = 1;
     // const filterPage = `&name=${req.query.name}&height=${req.query.height}&weight=${req.query.weight}&startDate=${req.query.startDate}&endDate=${req.query.endDate}&married=${req.query.married}&operation=${req.query.operation}`
-    const filter = {name: req.query.name,
-      height: req.query.height,
-      weight: req.query.weight,
+    const filter = {title: req.query.title,
+      complete: req.query.complete,
       startDate: req.query.startDate,
       endDate: req.query.endDate,
-      married: req.query.married,
       operation: req.query.operation
     }
 
-    if (req.query.name) {
-        wheres.push(`name like '%' || ? || '%'`);
-        values.push(req.query.name);
-        filterPageArray.push(`&name=${req.query.name}`)
+    if (req.query.title) {
+        wheres.push(`title ilike '%' || $${count++} || '%'`);
+        values.push(req.query.title);
+        filterPageArray.push(`&title=${req.query.title}`)
     }
 
-    if (req.query.height) {
-        wheres.push(`height = ?`);
-        values.push(req.query.height);
-        filterPageArray.push(`&height=${req.query.height}`)
-    }
-
-    if (req.query.weight) {
-        wheres.push(`weight = ?`);
-        values.push(req.query.weight);
-        filterPageArray.push(`&weight=${req.query.weight}`)
+    if (req.query.complete) {
+        wheres.push(`complete = $${count++}`);
+        values.push(req.query.complete);
+        filterPageArray.push(`&complete=${req.query.complete}`)
     }
 
     if (req.query.startDate || req.query.endDate ) {
         if (req.query.startDate && req.query.endDate) {
-            wheres.push('birthdate BETWEEN ? AND ?')
+            wheres.push(`deadline BETWEEN $${count++} AND $${count++}`)
             values.push(req.query.startDate);
             values.push(req.query.endDate);
             filterPageArray.push(`&startDate=${req.query.startDate}`)
             filterPageArray.push(`&endDate=${req.query.endDate}`)
         }
         else if (req.query.startDate) {
-            wheres.push('birthdate >= ?')
+            wheres.push(`deadline >= $${count++}`)
             values.push(req.query.startDate);
             filterPageArray.push(`&startDate=${req.query.startDate}`)
         }
         else if (req.query.endDate) {
-            wheres.push('birthdate <= ?')
+            wheres.push(`deadline <= $${count++}`)
             values.push(req.query.endDate);
             filterPageArray.push(`&endDate=${req.query.endDate}`)
         }
     }
 
-    if (req.query.married) {
-        wheres.push(`married = ?`);
-        values.push(req.query.married);
-        filterPageArray.push(`&married=${req.query.married}`)
-    }
 
     if (req.query.operation){
       filterPageArray.push(`&operation=${req.query.operation}`)
@@ -112,30 +100,38 @@ module.exports = function (db) {
     // console.log(values)
     // console.log(wheres)
 
-    db.all(sql, values, (err, data) => {
-        if (err) {
-            console.error(err);
-        }
-        // console.log(data)
-        const pages = Math.ceil(data[0].total / limit)
-        sql = 'SELECT * FROM data'
-        if (wheres.length > 0) {
-          if (req.query.operation == 'OR'){
-            sql += ` WHERE ${wheres.join(' OR ')}`
-          } else if (req.query.operation == 'AND'){
-            sql += ` WHERE ${wheres.join(' AND ')}`
-          }
-        }
-        sql += ' LIMIT ? OFFSET ?'
-        filterPage += filterPageArray.join('')
-        db.all(sql, [...values, limit, offset], (err, data) => {
-            if (err) {
-                console.error(err);
-            }
-            console.log(page, pages)
-            console.log(parseInt(page) === pages)
-            res.render('index', { rows: data, pages, page, filter, filterPage })
-        })
+    // db.query(sql, values, (err, data) => {
+    //     if (err) {
+    //         console.error(err);
+    //     }
+    //     // console.log(data)
+    //     const pages = Math.ceil(data[0].total / limit)
+    //     sql = 'SELECT * FROM data'
+    //     if (wheres.length > 0) {
+    //       if (req.query.operation == 'OR'){
+    //         sql += ` WHERE ${wheres.join(' OR ')}`
+    //       } else if (req.query.operation == 'AND'){
+    //         sql += ` WHERE ${wheres.join(' AND ')}`
+    //       }
+    //     }
+    //     sql += ' LIMIT ? OFFSET ?'
+    //     filterPage += filterPageArray.join('')
+    //     db.query(sql, [...values, limit, offset], (err, data) => {
+    //         if (err) {
+    //             console.error(err);
+    //         }
+    //         console.log(page, pages)
+    //         console.log(parseInt(page) === pages)
+    //         res.render('todos', { rows: data, pages, page, filter, filterPage })
+    //     })
+    // })
+
+    db.query(sql, (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log(data.rows);
+      res.render('todos', { rows: data.rows, pages, page, filter, filterPage })
     })
 
   });
